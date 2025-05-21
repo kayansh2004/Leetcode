839aa8e2452c3b9f0a48112f3d666b6d24b72795
kayansh2004 <153160877+kayansh2004@users.noreply.github.com>
2025-05-21 13:14:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (83.59%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } else {
+                if (stk.empty()) return false;
+
+                char top = stk.top();
+                if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
